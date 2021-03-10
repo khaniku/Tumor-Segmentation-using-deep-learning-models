e842b24bec5f09a169bb9bc6d125cb71026b3e3e
Emeka Kanikwu <kanikwuemeka@gmail.com>
2021-03-10 10:56:38 -0600

    updated model checkpoint

--- trainers/pytorch_trainer.py
+++ trainers/pytorch_trainer.py
@@ -123,9 +123,7 @@ class PytorchTrainer(TrainerBase, ABC):
             if self.i_step % verbose_step_num == 0:
                 print(f'epoch: {self.i_step / self.dataset_size:.2f}', log_dict)
                 self.save()
-            else:
-                print(f'epoch: {self.i_step / self.dataset_size:.2f}', log_dict)
-                self.save()
+
                 # metrics = self._validate(
                 #     validation_data_generator, metric, batch_size=batch_size
                 # )
